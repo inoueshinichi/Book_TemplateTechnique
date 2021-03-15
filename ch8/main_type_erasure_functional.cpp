865f1dcb5077977167e2015ef7b0a9c3ff0cd1cc
Inoue Shinichi XPS13 9310 on Ubuntu 20.04 LTS <inoue.shinichi.1800@gmail.com>
2021-03-15 11:38:13 +0900

    add drawable class with std::function of draw()

--- ch8/main_type_erasure_functional.cpp
+++ ch8/main_type_erasure_functional.cpp
@@ -15,6 +15,41 @@ struct functor {
     }
 };
 
+// std::function<>の応用
+// draw()というメンバ関数を持っていれば、あらゆる型で振る舞えるdrawableクラス
+class drawable {
+    std::function<void()> draw_func_;
+public:
+    drawable() {}
+
+    template <class T>
+    drawable(T& obj)
+        : draw_func_([&obj]() { obj.draw(); }) {}
+    
+    template <class T>
+    drawable& operator=(T& obj) {
+        draw_func_ = [&obj]() { obj.draw(); };
+        return *this;
+    }
+
+    void draw() const {
+        draw_func_();
+    }
+};
+
+struct circle {
+    void draw() const {
+        std::cout << "円を描画する" << std::endl;
+    }
+};
+
+struct square {
+    void draw() const {
+        std::cout << "矩形を描画する" << std::endl;
+    }
+};
+
+
 int main(int, char**)
 {
     // std::function<>の使い方
@@ -62,4 +97,18 @@ int main(int, char**)
         std::cout << functor_result << std::endl;
     }
 
+
+    // std::function<>の応用
+    // draw()というメンバ関数を持っていれば、あらゆる型で振る舞えるdrawableクラス
+    {
+        drawable a;
+
+        circle c;
+        a = c; // operator=
+        a.draw();
+
+        square s;
+        a = s; // operator=
+        a.draw();
+    }
 }
